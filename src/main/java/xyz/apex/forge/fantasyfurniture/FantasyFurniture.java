8be07ab160828a75c70324055baf31b7c6aa1206
Robson Lawson <29412632+apexmodder@users.noreply.github.com>
2022-02-12 16:44:59 +0000

    Initial source commit
    Signed-off-by: ApexModder <robsonlawson3@gmail.com>

---
+++ src/main/java/xyz/apex/forge/fantasyfurniture/FantasyFurniture.java
@@ -0,0 +1,9 @@
+package xyz.apex.forge.fantasyfurniture;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(FantasyFurniture.ID)
+public class FantasyFurniture
+{
+	public static final String ID = "fantasyfurniture";
+}
--- src/main/java/xyz/apex/forge/template/TemplateMod.java
+++
@@ -1,9 +0,0 @@
-package xyz.apex.forge.template;
-
-import net.minecraftforge.fml.common.Mod;
-
-@Mod(TemplateMod.ID)
-public class TemplateMod
-{
-	public static final String ID = "template";
-}
