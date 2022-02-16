5833a2584b53f92a540f388b11f0ee9c27c9398b
ApexModder <robsonlawson3@gmail.com>
2022-02-16 07:10:32 +0000

    Small refactor, added new funiture assets
    Signed-off-by: ApexModder <robsonlawson3@gmail.com>

--- src/main/java/xyz/apex/forge/fantasyfurniture/init/FFBlocks.java
+++ src/main/java/xyz/apex/forge/fantasyfurniture/init/FFBlocks.java
@@ -14,8 +14,7 @@ import net.minecraft.loot.conditions.BlockStateProperty;
 import net.minecraft.loot.conditions.SurvivesExplosion;
 import net.minecraft.state.Property;
 import net.minecraft.state.properties.BedPart;
-import net.minecraft.tags.BlockTags;
-import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.ITag;
 import net.minecraft.util.IItemProvider;
 import net.minecraft.util.IStringSerializable;
 import net.minecraft.village.PointOfInterestType;
@@ -23,7 +22,10 @@ import net.minecraft.village.PointOfInterestType;
 import xyz.apex.forge.apexcore.lib.util.reflection.FieldHelper;
 import xyz.apex.forge.fantasyfurniture.FantasyFurniture;
 import xyz.apex.forge.fantasyfurniture.block.NordicBedSingleBlock;
+import xyz.apex.forge.utility.registrator.builder.BlockBuilder;
 import xyz.apex.forge.utility.registrator.entry.BlockEntry;
+import xyz.apex.forge.utility.registrator.factory.BlockFactory;
+import xyz.apex.repack.com.tterrag.registrate.providers.RegistrateLangProvider;
 
 import java.util.Set;
 
@@ -33,38 +35,48 @@ public final class FFBlocks
 {
 	private static final FFRegistry REGISTRY = FFRegistry.getInstance();
 
-	public static final BlockEntry<NordicBedSingleBlock> NORDIC_BED_SINGLE = REGISTRY
-			.block("nordic_bed_single", NordicBedSingleBlock::new)
-				.lang("Nordic Bed Single")
-				.lang(EN_GB, "Nordic Bed Single")
-
-				.initialProperties(Material.WOOL, MaterialColor.WOOL)
-				.sound(SoundType.WOOD)
-				.strength(.2F)
-				.noOcclusion()
-
-				.blockState((ctx, provider) -> {
-					provider.horizontalBlock(ctx.get(), provider.models().getExistingFile(Names.NORDIC_BED_SINGLE_TEMPLATE), 0);
-				})
-				.loot((lootTables, block) -> lootTables.add(block, createSinglePropConditionTable(block, NordicBedSingleBlock.PART, BedPart.HEAD)))
-				.recipe((ctx, provider) -> {
-					// TODO:
-				})
-				.tag(BlockTags.BEDS)
-
-				.addRenderType(() -> RenderType::cutout)
-
-				.item(BedItem::new)
-					.stacksTo(1)
-
-					.model((ctx, provider) -> provider.withExistingParent(ctx.getName(), Names.NORDIC_BED_SINGLE_TEMPLATE))
-					.tag(ItemTags.BEDS)
-				.build()
+	// region: Nordic
+	// region: Bed Single
+	public static final BlockEntry<NordicBedSingleBlock> NORDIC_BED_SINGLE = singleBed("nordic", NordicBedSingleBlock::new, FFTags.Blocks.NORDIC, FFTags.Items.NORDIC)
 			.register();
+	// endregion
+	// endregion
 
 	static void bootstrap()
 	{
 		FantasyFurniture.registerPoiBlock(PointOfInterestType.HOME, NORDIC_BED_SINGLE, blockState -> blockState.getValue(NordicBedSingleBlock.PART) == BedPart.HEAD);
+	}
+
+	private static <BLOCK extends Block> BlockBuilder<FFRegistry, BLOCK, FFRegistry> singleBed(String type, BlockFactory<BLOCK> blockFactory, ITag.INamedTag<Block> blockTag, ITag.INamedTag<Item> itemTag)
+	{
+		return REGISTRY
+				.block(type + "_bed_singe", blockFactory)
+					.lang(RegistrateLangProvider.toEnglishName(type) + " Bed Single")
+					.lang(EN_GB, RegistrateLangProvider.toEnglishName(type) + " Bed Single")
+
+					.initialProperties(Material.WOOL, MaterialColor.WOOL)
+					.sound(SoundType.WOOD)
+					.strength(.2F)
+					.noOcclusion()
+
+					.blockState((ctx, provider) -> {
+						provider.horizontalBlock(ctx.get(), provider.models().getExistingFile(REGISTRY.id("block/" + type + "/bed_single")), 0);
+					})
+					.loot((lootTables, block) -> lootTables.add(block, createSinglePropConditionTable(block, NordicBedSingleBlock.PART, BedPart.HEAD)))
+					.recipe((ctx, provider) -> {
+						// TODO:
+					})
+					.tag(blockTag, FFTags.Blocks.BEDS)
+
+					.addRenderType(() -> RenderType::cutout)
+
+					.item(BedItem::new)
+						.stacksTo(1)
+
+						.model((ctx, provider) -> provider.withExistingParent(ctx.getName(), REGISTRY.id("block/" + type + "/bed_single")))
+						.tag(itemTag, FFTags.Items.BEDS)
+					.build()
+				;
 	}
 
 	private static final Set<Item> EXPLOSION_RESISTANT = FieldHelper.getPrivateValue(BlockLootTables.class, null, "EXPLOSION_RESISTANT");
--- src/main/java/xyz/apex/forge/fantasyfurniture/init/FFTags.java
+++ src/main/java/xyz/apex/forge/fantasyfurniture/init/FFTags.java
@@ -1,5 +1,11 @@
 package xyz.apex.forge.fantasyfurniture.init;
 
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ITag;
+import net.minecraft.tags.ItemTags;
+
 import static xyz.apex.forge.utility.registrator.AbstractRegistrator.BLOCK_ENTITY_TAGS_PROVIDER;
 import static xyz.apex.repack.com.tterrag.registrate.providers.ProviderType.*;
 
@@ -17,20 +23,56 @@ public final class FFTags
 
 	public static final class Blocks
 	{
+		public static final ITag.INamedTag<Block> NORDIC = REGISTRY.blockTagModded("nordic");
+		public static final ITag.INamedTag<Block> DECORATIONS = REGISTRY.blockTagModded("decorations");
+
+		public static final ITag.INamedTag<Block> BEDS = REGISTRY.blockTagModded("beds");
+		public static final ITag.INamedTag<Block> CHAIRS = REGISTRY.blockTagModded("chairs");
+		public static final ITag.INamedTag<Block> CUSHIONS = REGISTRY.blockTagModded("cushions");
+		public static final ITag.INamedTag<Block> SHELVES = REGISTRY.blockTagModded("shelves");
+		public static final ITag.INamedTag<Block> TABLES = REGISTRY.blockTagModded("tables");
+
 		private static void bootstrap()
 		{
 			REGISTRY.addDataGenerator(BLOCK_TAGS, provider -> {
+				provider.tag(NORDIC);
+				provider.tag(DECORATIONS);
 
+				provider.tag(BEDS);
+				provider.tag(CHAIRS);
+				provider.tag(CUSHIONS);
+				provider.tag(SHELVES);
+				provider.tag(TABLES);
+
+				provider.tag(BlockTags.BEDS).addTag(BEDS);
 			});
 		}
 	}
 
 	public static final class Items
 	{
+		public static final ITag.INamedTag<Item> NORDIC = REGISTRY.itemTagModded("nordic");
+		public static final ITag.INamedTag<Item> DECORATIONS = REGISTRY.itemTagModded("decorations");
+
+		public static final ITag.INamedTag<Item> BEDS = REGISTRY.itemTagModded("beds");
+		public static final ITag.INamedTag<Item> CHAIRS = REGISTRY.itemTagModded("chairs");
+		public static final ITag.INamedTag<Item> CUSHIONS = REGISTRY.itemTagModded("cushions");
+		public static final ITag.INamedTag<Item> SHELVES = REGISTRY.itemTagModded("shelves");
+		public static final ITag.INamedTag<Item> TABLES = REGISTRY.itemTagModded("tables");
+
 		private static void bootstrap()
 		{
 			REGISTRY.addDataGenerator(ITEM_TAGS, provider -> {
+				provider.tag(NORDIC);
+				provider.tag(DECORATIONS);
 
+				provider.tag(BEDS);
+				provider.tag(CHAIRS);
+				provider.tag(CUSHIONS);
+				provider.tag(SHELVES);
+				provider.tag(TABLES);
+
+				provider.tag(ItemTags.BEDS).addTag(BEDS);
 			});
 		}
 	}
