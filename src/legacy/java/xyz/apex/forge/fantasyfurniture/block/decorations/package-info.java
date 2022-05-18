a65d5dc1ba739a83de0d416cedd23ebd36709edb
ApexModder <robsonlawson3@gmail.com>
2022-05-18 05:56:36 +0100

    Remove old legacy code
    Signed-off-by: ApexModder <robsonlawson3@gmail.com>

--- src/legacy/java/xyz/apex/forge/fantasyfurniture/block/decorations/package-info.java
+++
@@ -1,6 +0,0 @@
-@MethodsReturnNonnullByDefault @FieldsAreNonnullByDefault @ParametersAreNonnullByDefault
-package xyz.apex.forge.fantasyfurniture.block.decorations;
-
-import xyz.apex.java.utility.nullness.FieldsAreNonnullByDefault;
-import xyz.apex.java.utility.nullness.MethodsReturnNonnullByDefault;
-import xyz.apex.java.utility.nullness.ParametersAreNonnullByDefault;
