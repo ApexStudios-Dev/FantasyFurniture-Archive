caab66a2b6bb7aff1020f400985259a502712188
ApexModder <robsonlawson3@gmail.com>
2022-06-26 10:54:14 +0100

    Delete old files, Clean slate
    Signed-off-by: ApexModder <robsonlawson3@gmail.com>

--- src/main/java/xyz/apex/forge/fantasyfurniture/FantasyFurniture.java
+++ src/main/java/xyz/apex/forge/fantasyfurniture/FantasyFurniture.java
@@ -3,47 +3,19 @@ package xyz.apex.forge.fantasyfurniture;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 
-import xyz.apex.forge.apexcore.lib.net.NetworkManager;
-import xyz.apex.forge.apexcore.lib.util.EventBusHelper;
-import xyz.apex.forge.apexcore.lib.util.InterModUtil;
 import xyz.apex.forge.commonality.Mods;
-import xyz.apex.forge.fantasyfurniture.client.renderer.model.WidowBloomModel;
-import xyz.apex.forge.fantasyfurniture.init.FFRegistry;
-import xyz.apex.forge.fantasyfurniture.init.FurnitureStation;
-import xyz.apex.forge.fantasyfurniture.net.SyncFurnitureStationResultsPacket;
 
 @Mod(Mods.FANTASY_FURNITURE)
 public final class FantasyFurniture
 {
 	public static final Logger LOGGER = LogManager.getLogger();
-	public static final String NBT_BLOCK_STATE_TAG = "BlockStateTag";
-	public static final NetworkManager NETWORK = new NetworkManager(Mods.FANTASY_FURNITURE, "net", "1");
 
 	public FantasyFurniture()
 	{
-		FFRegistry.bootstrap();
-		EventBusHelper.addEnqueuedListener(FMLCommonSetupEvent.class, event -> NETWORK.registerPacket(SyncFurnitureStationResultsPacket.class));
-
-		EventBusHelper.addEnqueuedListener(InterModProcessEvent.class, event -> {
-			event.getIMCStream(str -> str.equals(InterModUtil.FURNITURE_STATION_METHOD)).forEach(imc -> {
-				var obj = imc.getMessageSupplier().get();
-
-				if(obj instanceof ItemStack stack)
-				{
-					LOGGER.info("Received Furniture Station Result ('{}') from Mod: '{}'", stack.getItem().getRegistryName(), imc.getSenderModId());
-					FurnitureStation.registerAdditionalCraftingResult(stack);
-				}
-			});
-		});
-
 		DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> Client::new);
 	}
 
@@ -51,7 +23,6 @@ public final class FantasyFurniture
 	{
 		private Client()
 		{
-			EventBusHelper.addListener(EntityRenderersEvent.RegisterLayerDefinitions.class, event -> event.registerLayerDefinition(WidowBloomModel.LAYER_LOCATION, WidowBloomModel::createBodyLayer));
 		}
 	}
 }
--- src/main/java/xyz/apex/forge/fantasyfurniture/block/base/set/IFurnitureSetBlock.java
+++
@@ -1,8 +0,0 @@
-package xyz.apex.forge.fantasyfurniture.block.base.set;
-
-import xyz.apex.forge.fantasyfurniture.init.ModBlocks;
-
-public interface IFurnitureSetBlock
-{
-	ModBlocks getFurnitureSet();
-}
