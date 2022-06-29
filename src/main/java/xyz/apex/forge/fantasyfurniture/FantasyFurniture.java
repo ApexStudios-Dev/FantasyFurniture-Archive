15f2781f2f062332e3ba83409c66998d1e90de6f
ApexModder <robsonlawson3@gmail.com>
2022-06-29 11:19:51 +0100

    Attempt to alleviate issues when upgrading old worlds Add missing mappings event handler to remap old registry names to new registry names
    Signed-off-by: ApexModder <robsonlawson3@gmail.com>

--- src/main/java/xyz/apex/forge/fantasyfurniture/FantasyFurniture.java
+++ src/main/java/xyz/apex/forge/fantasyfurniture/FantasyFurniture.java
@@ -3,8 +3,12 @@ package xyz.apex.forge.fantasyfurniture;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
 
@@ -12,6 +16,8 @@ import xyz.apex.forge.apexcore.lib.net.NetworkManager;
 import xyz.apex.forge.apexcore.lib.util.EventBusHelper;
 import xyz.apex.forge.commonality.Mods;
 import xyz.apex.forge.fantasyfurniture.client.renderer.model.WidowBloomModel;
+import xyz.apex.forge.fantasyfurniture.init.ModBlocks;
+import xyz.apex.forge.fantasyfurniture.init.ModItems;
 import xyz.apex.forge.fantasyfurniture.init.ModRegistry;
 
 @Mod(Mods.FANTASY_FURNITURE)
@@ -24,6 +30,134 @@ public final class FantasyFurniture
 	{
 		ModRegistry.bootstrap();
 		DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> Client::new);
+
+		// EventBusHelper breaks when using MissingMappings event
+		// It registers to the mod bus since the event implements the IModBus interface
+		// although the event is fired on the forge bus, notify forge about this? (should the event even implement the interface? it's a marker to say what bus events should be registered to and fired on)
+		MinecraftForge.EVENT_BUS.addGenericListener(Block.class, this::onMissingBlockMappings);
+		MinecraftForge.EVENT_BUS.addGenericListener(Item.class, this::onMissingItemMappings);
+	}
+
+	private void onMissingBlockMappings(RegistryEvent.MissingMappings<Block> event)
+	{
+		var mappings = event.getMappings(Mods.FANTASY_FURNITURE);
+
+		if(!mappings.isEmpty())
+		{
+			mappings.forEach(RegistryEvent.MissingMappings.Mapping::fail);
+
+			for(var mapping : mappings)
+			{
+				switch(mapping.key.toString())
+				{
+					case "fantasyfurniture:bone/bed_double" -> mapping.remap(ModBlocks.BONE_SKELETON_BED_DOUBLE.get());
+					case "fantasyfurniture:bone/bed_single" -> mapping.remap(ModBlocks.BONE_SKELETON_BED_SINGLE.get());
+					case "fantasyfurniture:bone/bench" -> mapping.remap(ModBlocks.BONE_SKELETON_BENCH.get());
+					case "fantasyfurniture:bone/bookshelf" -> mapping.remap(ModBlocks.BONE_SKELETON_BOOKSHELF.get());
+					case "fantasyfurniture:bone/carpet" -> mapping.remap(ModBlocks.BONE_SKELETON_CARPET.get());
+					case "fantasyfurniture:bone/chair" -> mapping.remap(ModBlocks.BONE_SKELETON_CHAIR.get());
+					case "fantasyfurniture:bone/chandelier" -> mapping.remap(ModBlocks.BONE_SKELETON_CHANDELIER.get());
+					case "fantasyfurniture:bone/chest" -> mapping.remap(ModBlocks.BONE_SKELETON_CHEST.get());
+					case "fantasyfurniture:bone/cushion" -> mapping.remap(ModBlocks.BONE_SKELETON_SKULL.get());
+					case "fantasyfurniture:bone/desk_left" -> mapping.remap(ModBlocks.BONE_SKELETON_DESK_LEFT.get());
+					case "fantasyfurniture:bone/desk_right" -> mapping.remap(ModBlocks.BONE_SKELETON_DESK_RIGHT.get());
+					case "fantasyfurniture:bone/door_double" -> mapping.remap(ModBlocks.BONE_SKELETON_DOOR_DOUBLE.get());
+					case "fantasyfurniture:bone/door_single" -> mapping.remap(ModBlocks.BONE_SKELETON_DOOR_SINGLE.get());
+					case "fantasyfurniture:bone/drawer" -> mapping.remap(ModBlocks.BONE_SKELETON_DRAWER.get());
+					case "fantasyfurniture:bone/dresser" -> mapping.remap(ModBlocks.BONE_SKELETON_DRESSER.get());
+					case "fantasyfurniture:bone/floor_light" -> mapping.remap(ModBlocks.BONE_SKELETON_FLOOR_LIGHT.get());
+					case "fantasyfurniture:bone/lockbox" -> mapping.remap(ModBlocks.BONE_SKELETON_LOCKBOX.get());
+					case "fantasyfurniture:bone/painting_small" -> mapping.remap(ModBlocks.BONE_SKELETON_PAINTING_SMALL.get());
+					case "fantasyfurniture:bone/painting_wide" -> mapping.remap(ModBlocks.BONE_SKELETON_PAINTING_WIDE.get());
+					case "fantasyfurniture:bone/shelf" -> mapping.remap(ModBlocks.BONE_SKELETON_SHELF.get());
+					case "fantasyfurniture:bone/sofa" -> mapping.remap(ModBlocks.BONE_SKELETON_SOFA.get());
+					case "fantasyfurniture:bone/stool" -> mapping.remap(ModBlocks.BONE_SKELETON_STOOL.get());
+					case "fantasyfurniture:bone/table_large" -> mapping.remap(ModBlocks.BONE_SKELETON_TABLE_LARGE.get());
+					case "fantasyfurniture:bone/table_small" -> mapping.remap(ModBlocks.BONE_SKELETON_TABLE_SMALL.get());
+					case "fantasyfurniture:bone/table_wide" -> mapping.remap(ModBlocks.BONE_SKELETON_TABLE_WIDE.get());
+					case "fantasyfurniture:bone/wall_light" -> mapping.remap(ModBlocks.BONE_SKELETON_WALL_LIGHT.get());
+					case "fantasyfurniture:bone/wardrobe_bottom" -> mapping.remap(ModBlocks.BONE_SKELETON_WARDROBE_BOTTOM.get());
+					case "fantasyfurniture:bone/wardrobe_top" -> mapping.remap(ModBlocks.BONE_SKELETON_WARDROBE_TOP.get());
+					case "fantasyfurniture:bone/wool" -> mapping.remap(ModBlocks.BONE_SKELETON_WOOL.get());
+					case "fantasyfurniture:decorations/dunmer/pottery_0" -> mapping.remap(ModBlocks.DUNMER_POTTERY_0.get());
+					case "fantasyfurniture:decorations/dunmer/pottery_1" -> mapping.remap(ModBlocks.DUNMER_POTTERY_1.get());
+					case "fantasyfurniture:decorations/nordic/boiled_creme_treats" -> mapping.remap(ModBlocks.BOILED_CREME_TREATS.get());
+					case "fantasyfurniture:decorations/nordic/mead_bottles" -> mapping.remap(ModBlocks.MEAD_BOTTLES.get());
+					case "fantasyfurniture:decorations/nordic/soul_gems_dark" -> mapping.remap(ModBlocks.NORDIC_SOUL_GEMS_DARK.get());
+					case "fantasyfurniture:decorations/nordic/soul_gems_light" -> mapping.remap(ModBlocks.NORDIC_SOUL_GEMS_LIGHT.get());
+					case "fantasyfurniture:decorations/nordic/sweetrolls" -> mapping.remap(ModBlocks.SWEETROLLS.get());
+					case "fantasyfurniture:decorations/venthyr/chalices" -> mapping.remap(ModBlocks.VENTHYR_CHALICES.get());
+					case "fantasyfurniture:decorations/venthyr/food_0" -> mapping.remap(ModBlocks.VENTHYR_FOOD_0.get());
+					case "fantasyfurniture:decorations/venthyr/food_1" -> mapping.remap(ModBlocks.VENTHYR_FOOD_1.get());
+					case "fantasyfurniture:decorations/venthyr/platter" -> mapping.remap(ModBlocks.VENTHYR_PLATTER.get());
+					case "fantasyfurniture:decorations/venthyr/tea_cups" -> mapping.remap(ModBlocks.VENTHYR_TEA_CUPS.get());
+					case "fantasyfurniture:decorations/venthyr/tea_set" -> mapping.remap(ModBlocks.VENTHYR_TEA_SET.get());
+					case "fantasyfurniture:decorations/venthyr/tomes" -> mapping.remap(ModBlocks.VENTHYR_TOMES.get());
+					case "fantasyfurniture:decorations/venthyr/widow_bloom" -> mapping.remap(ModBlocks.VENTHYR_WIDOW_BLOOM.get());
+				}
+			}
+		}
+	}
+
+	private void onMissingItemMappings(RegistryEvent.MissingMappings<Item> event)
+	{
+		var mappings = event.getMappings(Mods.FANTASY_FURNITURE);
+
+		if(!mappings.isEmpty())
+		{
+			mappings.forEach(RegistryEvent.MissingMappings.Mapping::fail);
+
+			for(var mapping : mappings)
+			{
+				switch(mapping.key.toString())
+				{
+					case "fantasyfurniture:bone/bed_double" -> mapping.remap(ModItems.BONE_SKELETON_BED_DOUBLE.get());
+					case "fantasyfurniture:bone/bed_single" -> mapping.remap(ModItems.BONE_SKELETON_BED_SINGLE.get());
+					case "fantasyfurniture:bone/bench" -> mapping.remap(ModItems.BONE_SKELETON_BENCH.get());
+					case "fantasyfurniture:bone/bookshelf" -> mapping.remap(ModItems.BONE_SKELETON_BOOKSHELF.get());
+					case "fantasyfurniture:bone/carpet" -> mapping.remap(ModItems.BONE_SKELETON_CARPET.get());
+					case "fantasyfurniture:bone/chair" -> mapping.remap(ModItems.BONE_SKELETON_CHAIR.get());
+					case "fantasyfurniture:bone/chandelier" -> mapping.remap(ModItems.BONE_SKELETON_CHANDELIER.get());
+					case "fantasyfurniture:bone/chest" -> mapping.remap(ModItems.BONE_SKELETON_CHEST.get());
+					case "fantasyfurniture:bone/cushion" -> mapping.remap(ModItems.BONE_SKELETON_SKULL.get());
+					case "fantasyfurniture:bone/desk_left" -> mapping.remap(ModItems.BONE_SKELETON_DESK_LEFT.get());
+					case "fantasyfurniture:bone/desk_right" -> mapping.remap(ModItems.BONE_SKELETON_DESK_RIGHT.get());
+					case "fantasyfurniture:bone/door_double" -> mapping.remap(ModItems.BONE_SKELETON_DOOR_DOUBLE.get());
+					case "fantasyfurniture:bone/door_single" -> mapping.remap(ModItems.BONE_SKELETON_DOOR_SINGLE.get());
+					case "fantasyfurniture:bone/drawer" -> mapping.remap(ModItems.BONE_SKELETON_DRAWER.get());
+					case "fantasyfurniture:bone/dresser" -> mapping.remap(ModItems.BONE_SKELETON_DRESSER.get());
+					case "fantasyfurniture:bone/floor_light" -> mapping.remap(ModItems.BONE_SKELETON_FLOOR_LIGHT.get());
+					case "fantasyfurniture:bone/lockbox" -> mapping.remap(ModItems.BONE_SKELETON_LOCKBOX.get());
+					case "fantasyfurniture:bone/painting_small" -> mapping.remap(ModItems.BONE_SKELETON_PAINTING_SMALL.get());
+					case "fantasyfurniture:bone/painting_wide" -> mapping.remap(ModItems.BONE_SKELETON_PAINTING_WIDE.get());
+					case "fantasyfurniture:bone/shelf" -> mapping.remap(ModItems.BONE_SKELETON_SHELF.get());
+					case "fantasyfurniture:bone/sofa" -> mapping.remap(ModItems.BONE_SKELETON_SOFA.get());
+					case "fantasyfurniture:bone/stool" -> mapping.remap(ModItems.BONE_SKELETON_STOOL.get());
+					case "fantasyfurniture:bone/table_large" -> mapping.remap(ModItems.BONE_SKELETON_TABLE_LARGE.get());
+					case "fantasyfurniture:bone/table_small" -> mapping.remap(ModItems.BONE_SKELETON_TABLE_SMALL.get());
+					case "fantasyfurniture:bone/table_wide" -> mapping.remap(ModItems.BONE_SKELETON_TABLE_WIDE.get());
+					case "fantasyfurniture:bone/wall_light" -> mapping.remap(ModItems.BONE_SKELETON_WALL_LIGHT.get());
+					case "fantasyfurniture:bone/wardrobe_bottom" -> mapping.remap(ModItems.BONE_SKELETON_WARDROBE_BOTTOM.get());
+					case "fantasyfurniture:bone/wardrobe_top" -> mapping.remap(ModItems.BONE_SKELETON_WARDROBE_TOP.get());
+					case "fantasyfurniture:bone/wool" -> mapping.remap(ModItems.BONE_SKELETON_WOOL.get());
+					case "fantasyfurniture:decorations/dunmer/pottery_0" -> mapping.remap(ModItems.DUNMER_POTTERY_0.get());
+					case "fantasyfurniture:decorations/dunmer/pottery_1" -> mapping.remap(ModItems.DUNMER_POTTERY_1.get());
+					case "fantasyfurniture:decorations/nordic/boiled_creme_treats" -> mapping.remap(ModItems.BOILED_CREME_TREATS.get());
+					case "fantasyfurniture:decorations/nordic/mead_bottles" -> mapping.remap(ModItems.MEAD_BOTTLES.get());
+					case "fantasyfurniture:decorations/nordic/soul_gems_dark" -> mapping.remap(ModItems.NORDIC_SOUL_GEMS_DARK.get());
+					case "fantasyfurniture:decorations/nordic/soul_gems_light" -> mapping.remap(ModItems.NORDIC_SOUL_GEMS_LIGHT.get());
+					case "fantasyfurniture:decorations/nordic/sweetrolls" -> mapping.remap(ModItems.SWEETROLLS.get());
+					case "fantasyfurniture:decorations/venthyr/chalices" -> mapping.remap(ModItems.VENTHYR_CHALICES.get());
+					case "fantasyfurniture:decorations/venthyr/food_0" -> mapping.remap(ModItems.VENTHYR_FOOD_0.get());
+					case "fantasyfurniture:decorations/venthyr/food_1" -> mapping.remap(ModItems.VENTHYR_FOOD_1.get());
+					case "fantasyfurniture:decorations/venthyr/platter" -> mapping.remap(ModItems.VENTHYR_PLATTER.get());
+					case "fantasyfurniture:decorations/venthyr/tea_cups" -> mapping.remap(ModItems.VENTHYR_TEA_CUPS.get());
+					case "fantasyfurniture:decorations/venthyr/tea_set" -> mapping.remap(ModItems.VENTHYR_TEA_SET.get());
+					case "fantasyfurniture:decorations/venthyr/tomes" -> mapping.remap(ModItems.VENTHYR_TOMES.get());
+					case "fantasyfurniture:decorations/venthyr/widow_bloom" -> mapping.remap(ModItems.VENTHYR_WIDOW_BLOOM.get());
+				}
+			}
+		}
 	}
 
 	private static final class Client
