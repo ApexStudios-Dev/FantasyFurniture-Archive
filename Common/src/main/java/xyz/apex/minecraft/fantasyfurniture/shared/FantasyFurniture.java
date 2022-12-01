7fe160f0adf89db07ac7a1d870a0296befa4b6c1
Apex <robsonlawson3@gmail.com>
2022-12-01 02:03:50 +0000

    Move to use new Builder classes from ApexCore

--- Common/src/main/java/xyz/apex/minecraft/fantasyfurniture/shared/FantasyFurniture.java
+++ Common/src/main/java/xyz/apex/minecraft/fantasyfurniture/shared/FantasyFurniture.java
@@ -2,8 +2,12 @@ package xyz.apex.minecraft.fantasyfurniture.shared;
 
 import net.minecraft.core.particles.ParticleType;
 import net.minecraft.world.inventory.MenuType;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 
 import xyz.apex.minecraft.apexcore.shared.registry.ModdedRegistries;
 import xyz.apex.minecraft.apexcore.shared.registry.ModdedRegistry;
@@ -12,9 +16,16 @@ import xyz.apex.minecraft.apexcore.shared.registry.block.BlockRegistry;
 import xyz.apex.minecraft.apexcore.shared.registry.item.ItemRegistry;
 import xyz.apex.minecraft.fantasyfurniture.shared.init.*;
 
+import java.util.function.Supplier;
+
 public interface FantasyFurniture
 {
     String ID = "fantasyfurniture";
+
+    Supplier<Item.Properties> ITEM_PROPERTIES = () -> new Item.Properties().tab(CreativeModeTab.TAB_MISC);
+    Supplier<BlockBehaviour.Properties> STONE_PROPERTIES = () -> BlockBehaviour.Properties.copy(Blocks.STONE);
+    Supplier<BlockBehaviour.Properties> WOOL_PROPERTIES = () -> BlockBehaviour.Properties.copy(Blocks.WHITE_WOOL);
+    Supplier<BlockBehaviour.Properties> CARPET_PROPERTIES = () -> BlockBehaviour.Properties.copy(Blocks.WHITE_CARPET);
 
     static void bootstrap()
     {
@@ -32,10 +43,10 @@ public interface FantasyFurniture
     {
         BlockRegistry BLOCKS = BlockRegistry.create(ID);
         ItemRegistry ITEMS = ItemRegistry.create(ID);
-        ModdedRegistry<MenuType<?>> MENUS = ModdedRegistries.create(RegistryKeys.MENU, ID);
-        ModdedRegistry<ParticleType<?>> PARTICLE_TYPES = ModdedRegistries.create(RegistryKeys.PARTICLE_TYPE, ID);
-        ModdedRegistry<RecipeSerializer<?>> RECIPE_SERIALIZERS = ModdedRegistries.create(RegistryKeys.RECIPE_SERIALIZER, ID);
-        ModdedRegistry<RecipeType<?>> RECIPE_TYPES = ModdedRegistries.create(RegistryKeys.RECIPE_TYPE, ID);
+        ModdedRegistry<MenuType<?>> MENUS = ModdedRegistries.get(RegistryKeys.MENU, ID);
+        ModdedRegistry<ParticleType<?>> PARTICLE_TYPES = ModdedRegistries.get(RegistryKeys.PARTICLE_TYPE, ID);
+        ModdedRegistry<RecipeSerializer<?>> RECIPE_SERIALIZERS = ModdedRegistries.get(RegistryKeys.RECIPE_SERIALIZER, ID);
+        ModdedRegistry<RecipeType<?>> RECIPE_TYPES = ModdedRegistries.get(RegistryKeys.RECIPE_TYPE, ID);
 
         private static void bootstrap()
         {
--- Common/src/main/java/xyz/apex/minecraft/fantasyfurniture/shared/init/NordicSet.java
+++ Common/src/main/java/xyz/apex/minecraft/fantasyfurniture/shared/init/NordicSet.java
@@ -2,13 +2,17 @@ package xyz.apex.minecraft.fantasyfurniture.shared.init;
 
 import net.minecraft.world.level.block.Block;
 
+import xyz.apex.minecraft.apexcore.shared.registry.block.BlockBuilder;
 import xyz.apex.minecraft.apexcore.shared.registry.block.BlockRegistryEntry;
+import xyz.apex.minecraft.apexcore.shared.registry.item.ItemBuilder;
+import xyz.apex.minecraft.fantasyfurniture.shared.FantasyFurniture;
 
 public interface NordicSet
 {
-    BlockRegistryEntry<Block> WOOL = Builders.wool("nordic/wool");
+    BlockRegistryEntry<Block> WOOL = BlockBuilder.generic(FantasyFurniture.Registries.BLOCKS, "nordic/wool").initialProperties(FantasyFurniture.WOOL_PROPERTIES).register();
 
     static void bootstrap()
     {
+        ItemBuilder.forBlockGeneric(FantasyFurniture.Registries.ITEMS, WOOL).initialProperties(FantasyFurniture.ITEM_PROPERTIES).register();
     }
 }
