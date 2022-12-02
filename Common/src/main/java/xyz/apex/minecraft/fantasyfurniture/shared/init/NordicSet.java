3cb5a74634bf97d2093292413d19fb6f10ab618c
Apex <robsonlawson3@gmail.com>
2022-12-02 06:06:00 +0000

    Register Flammability values for Wool & Carpet blocks

--- Common/src/main/java/xyz/apex/minecraft/fantasyfurniture/shared/init/NordicSet.java
+++ Common/src/main/java/xyz/apex/minecraft/fantasyfurniture/shared/init/NordicSet.java
@@ -5,15 +5,31 @@ import net.minecraft.world.level.block.CarpetBlock;
 
 import xyz.apex.minecraft.apexcore.shared.registry.block.BlockRegistryEntry;
 import xyz.apex.minecraft.fantasyfurniture.shared.FantasyFurniture;
+import xyz.apex.minecraft.fantasyfurniture.shared.block.WallLightBlock;
 
 public interface NordicSet
 {
-    BlockRegistryEntry<Block> WOOL = FantasyFurniture.Registries.BLOCKS.generic("nordic/wool").initialProperties(FantasyFurniture.WOOL_PROPERTIES).register();
-    BlockRegistryEntry<CarpetBlock> CARPET = FantasyFurniture.Registries.BLOCKS.builder("nordic/carpet", CarpetBlock::new).initialProperties(FantasyFurniture.CARPET_PROPERTIES).register();
+    BlockRegistryEntry<Block> WOOL = FantasyFurniture.Registries.BLOCKS
+            .generic("nordic/wool")
+            .flammability(30, 60)
+            .initialProperties(FantasyFurniture.WOOL_PROPERTIES)
+    .register();
+
+    BlockRegistryEntry<CarpetBlock> CARPET = FantasyFurniture.Registries.BLOCKS
+            .builder("nordic/carpet", CarpetBlock::new)
+            .flammability(60, 20)
+            .initialProperties(FantasyFurniture.CARPET_PROPERTIES)
+    .register();
+
+    BlockRegistryEntry<WallLightBlock> WALL_LIGHT = FantasyFurniture.Registries.BLOCKS
+            .builder("nordic/carpet", WallLightBlock::torchLike)
+            .initialProperties(FantasyFurniture.TORCH_PROPERTIES)
+    .register();
 
     static void bootstrap()
     {
         FantasyFurniture.Registries.ITEMS.genericBlockBuilder(WOOL).register();
         FantasyFurniture.Registries.ITEMS.genericBlockBuilder(CARPET).register();
+        FantasyFurniture.Registries.ITEMS.genericBlockBuilder(WALL_LIGHT).register();
     }
 }
