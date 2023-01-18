6871cf0a92ddb53f116aa779ecb2e4feabc1875c
Apex <robsonlawson3@gmail.com>
2023-01-18 23:53:39 +0000

    Add JEI dependency & plugin

---
+++ Fabric/src/main/java/xyz/apex/minecraft/fantasyfurniture/fabric/integrations/jei/JeiFantasyFurnitureFabricPlugin.java
@@ -0,0 +1,5 @@
+package xyz.apex.minecraft.fantasyfurniture.fabric.integrations.jei;
+
+import xyz.apex.minecraft.fantasyfurniture.shared.integrations.jei.JeiFantasyFurniturePlugin;
+
+public final class JeiFantasyFurnitureFabricPlugin implements JeiFantasyFurniturePlugin {}
---
+++ Quilt/src/main/java/xyz/apex/minecraft/fantasyfurniture/quilt/integrations/jei/JeiFantasyFurnitureQuiltPlugin.java
@@ -0,0 +1,5 @@
+package xyz.apex.minecraft.fantasyfurniture.quilt.integrations.jei;
+
+import xyz.apex.minecraft.fantasyfurniture.shared.integrations.jei.JeiFantasyFurniturePlugin;
+
+public final class JeiFantasyFurnitureQuiltPlugin implements JeiFantasyFurniturePlugin {}
