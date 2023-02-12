3f2d412e37f3667dcdbd0e575a785835e11f2ad2
Apex <robsonlawson3@gmail.com>
2023-02-12 05:05:58 +0000

    Apply particle & sound effects to oven block when active

--- common/src/main/java/xyz/apex/minecraft/fantasyfurniture/common/block/OvenBlock.java
+++ common/src/main/java/xyz/apex/minecraft/fantasyfurniture/common/block/OvenBlock.java
@@ -3,6 +3,11 @@ package xyz.apex.minecraft.fantasyfurniture.common.block;
 import org.jetbrains.annotations.Nullable;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.AbstractFurnaceBlock;
@@ -12,6 +17,7 @@ import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 
 import xyz.apex.minecraft.fantasyfurniture.common.init.AllBlockEntityTypes;
+import xyz.apex.minecraft.fantasyfurniture.common.init.NordicSet;
 
 public class OvenBlock extends AbstractFurnaceBlock
 {
@@ -38,5 +44,29 @@ public class OvenBlock extends AbstractFurnaceBlock
     public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState blockState, BlockEntityType<T> blockEntityType)
     {
         return createFurnaceTicker(level, blockEntityType, AllBlockEntityTypes.OVEN.get());
+    }
+
+    @Override
+    public void animateTick(BlockState blockState, Level level, BlockPos pos, RandomSource random)
+    {
+        if(!blockState.getValue(LIT)) return;
+
+        if(NordicSet.OVEN.hasBlockState(blockState))
+        {
+            var x = pos.getX() + .5D;
+            var y = pos.getY();
+            var z = pos.getZ() + .5D;
+
+            if(random.nextDouble() < .1D) level.playLocalSound(x, y, z, SoundEvents.SMOKER_SMOKE, SoundSource.BLOCKS, 1F, 1F, false);
+
+            var facing = blockState.getValue(FACING);
+            var axis = facing.getAxis();
+            var h = random.nextDouble() * .6D - .3D;
+            var xOff = axis == Direction.Axis.X ? facing.getStepX() * .52D : h;
+            var yOff = random.nextDouble() * 9D / 16D;
+            var zOff = axis == Direction.Axis.Z ? facing.getStepZ() * .52D : h;
+
+            level.addParticle(ParticleTypes.SMOKE, x + xOff, y + yOff, z + zOff, 0D, 0D, 0D);
+        }
     }
 }
