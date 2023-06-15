09e8d93ac85f3dd6e64dab9726169a1061a71270
Apex <robsonlawson3@gmail.com>
2023-06-15 22:19:54 +0100

    Clear out project - Fresh start

--- src/main/java/xyz/apex/forge/fantasyfurniture/client/screen/package-info.java
+++
@@ -1,5 +0,0 @@
-@javax.annotation.ParametersAreNonnullByDefault
-@net.minecraft.MethodsReturnNonnullByDefault
-@net.minecraft.FieldsAreNonnullByDefault
-@xyz.apex.forge.commonality.SideOnly(xyz.apex.forge.commonality.SideOnly.Side.CLIENT)
-package xyz.apex.forge.fantasyfurniture.client.screen;
--- src/main/java/xyz/apex/forge/fantasyfurniture/mixin/client/package-info.java
+++
@@ -1,4 +0,0 @@
-@javax.annotation.ParametersAreNonnullByDefault
-@net.minecraft.MethodsReturnNonnullByDefault
-@net.minecraft.FieldsAreNonnullByDefault
-package xyz.apex.forge.fantasyfurniture.mixin.client;
