52ef32518c89c8ea4d8356fc8ac1c7331100669b
Apex <robsonlawson3@gmail.com>
2023-08-18 06:21:16 +0100

    Add small, medium and large container blocks

--- projects/modules/nordic/common/src/main/java/xyz/apex/minecraft/fantasyfurniture/nordic/common/block/NordicDrawerBlock.java
+++ projects/modules/nordic/common/src/main/java/xyz/apex/minecraft/fantasyfurniture/nordic/common/block/NordicDrawerBlock.java
@@ -2,29 +2,16 @@ package xyz.apex.minecraft.fantasyfurniture.nordic.common.block;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.MenuProvider;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
-import org.jetbrains.annotations.Nullable;
-import xyz.apex.minecraft.apexcore.common.lib.component.block.BaseBlockComponentHolder;
 import xyz.apex.minecraft.apexcore.common.lib.component.block.BlockComponentRegistrar;
-import xyz.apex.minecraft.apexcore.common.lib.component.block.entity.types.BlockEntityComponentTypes;
 import xyz.apex.minecraft.apexcore.common.lib.component.block.types.BlockComponentTypes;
 import xyz.apex.minecraft.apexcore.common.lib.component.block.types.HorizontalFacingBlockComponent;
-import xyz.apex.minecraft.apexcore.common.lib.helper.InteractionResultHelper;
 import xyz.apex.minecraft.apexcore.common.lib.helper.VoxelShapeHelper;
-import xyz.apex.minecraft.apexcore.common.lib.hook.MenuHooks;
-import xyz.apex.minecraft.fantasyfurniture.nordic.common.NordicFurnitureSet;
 
-public final class NordicDrawerBlock extends BaseBlockComponentHolder
+public final class NordicDrawerBlock extends NordicSmallContainerBlock
 {
     public static final VoxelShape SHAPE = VoxelShapeHelper.combine(
             box(1D, 0D, 1D, 15D, 13D, 15D),
@@ -49,12 +36,6 @@ public final class NordicDrawerBlock extends BaseBlockComponentHolder
     }
 
     @Override
-    protected BlockEntityType<?> getBlockEntityType()
-    {
-        return NordicFurnitureSet.SMALL_CONTAINER_BLOCK_ENTITY.value();
-    }
-
-    @Override
     public VoxelShape getShape(BlockState blockState, BlockGetter level, BlockPos pos, CollisionContext context)
     {
         return switch(blockState.getValue(HorizontalFacingBlockComponent.FACING)) {
@@ -63,30 +44,5 @@ public final class NordicDrawerBlock extends BaseBlockComponentHolder
             case SOUTH -> SHAPE_SOUTH;
             case WEST -> SHAPE_WEST;
         };
-    }
-
-    @Override
-    public InteractionResult use(BlockState blockState, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit)
-    {
-        var result = super.use(blockState, level, pos, player, hand, hit);
-
-        if(result.consumesAction())
-            return result;
-
-        var blockEntity = NordicFurnitureSet.SMALL_CONTAINER_BLOCK_ENTITY.getBlockEntity(level, pos);
-
-        if(blockEntity == null)
-            return InteractionResultHelper.BlockUse.noActionTaken();
-
-        MenuHooks.get().openMenu(player, blockEntity.getRequiredComponent(BlockEntityComponentTypes.NAMEABLE).getName(), blockEntity, buffer -> { });
-        return InteractionResultHelper.BlockUse.succeedAndSwingArmBothSides(level.isClientSide);
-    }
-
-    @Nullable
-    @Override
-    public MenuProvider getMenuProvider(BlockState state, Level level, BlockPos pos)
-    {
-        var blockEntity = NordicFurnitureSet.SMALL_CONTAINER_BLOCK_ENTITY.getBlockEntity(level, pos);
-        return blockEntity == null ? null : MenuHooks.get().createMenuProvider(blockEntity.getRequiredComponent(BlockEntityComponentTypes.NAMEABLE).getName(), blockEntity, buffer -> { });
     }
 }
