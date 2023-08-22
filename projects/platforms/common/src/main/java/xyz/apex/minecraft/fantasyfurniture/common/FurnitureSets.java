dce49b7a6d7aa081bc27589b33413774405a4cbc
Apex <29412632+ApexModder@users.noreply.github.com>
2023-08-22 08:34:22 +0100

    Add nordic wood type

--- projects/platforms/common/src/main/java/xyz/apex/minecraft/fantasyfurniture/common/FurnitureSets.java
+++ projects/platforms/common/src/main/java/xyz/apex/minecraft/fantasyfurniture/common/FurnitureSets.java
@@ -12,6 +12,7 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.CarpetBlock;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.WoodType;
 import org.jetbrains.annotations.ApiStatus;
 import xyz.apex.minecraft.apexcore.common.core.ApexTags;
 import xyz.apex.minecraft.apexcore.common.lib.component.block.BaseBlockComponentHolder;
@@ -25,6 +26,7 @@ import xyz.apex.minecraft.apexcore.common.lib.registry.entry.BlockEntry;
 import xyz.apex.minecraft.apexcore.common.lib.registry.entry.ItemEntry;
 import xyz.apex.minecraft.apexcore.common.lib.registry.entry.RegistryEntry;
 import xyz.apex.minecraft.apexcore.common.lib.registry.factory.BlockFactory;
+import xyz.apex.minecraft.apexcore.common.lib.registry.generic.WoodTypeBuilder;
 import xyz.apex.minecraft.apexcore.common.lib.resgen.state.MultiVariantBuilder;
 import xyz.apex.minecraft.apexcore.common.lib.resgen.state.PropertyDispatch;
 import xyz.apex.minecraft.apexcore.common.lib.resgen.state.Variant;
@@ -35,6 +37,7 @@ import xyz.apex.minecraft.fantasyfurniture.common.block.entity.SmallContainerBlo
 import xyz.apex.minecraft.fantasyfurniture.common.block.property.ConnectionType;
 
 import java.util.function.Function;
+import java.util.function.UnaryOperator;
 
 @ApiStatus.NonExtendable
 public interface FurnitureSets
@@ -42,6 +45,16 @@ public interface FurnitureSets
     MultiBlockType MB_2x1x1 = MultiBlockType.builder().renderAtOriginOnly().with("XX").build();
     MultiBlockType MB_2x2x1 = MultiBlockType.builder().renderAtOriginOnly().with("XX").with("XX").build();
     MultiBlockType MB_1x2x1 = MultiBlockType.builder().renderAtOriginOnly().with("X").with("X").build();
+
+    static WoodType woodType(AbstractRegistrar<?> registrar, UnaryOperator<WoodTypeBuilder> builder)
+    {
+        return builder.apply(WoodTypeBuilder.builder()).register(new ResourceLocation(registrar.getOwnerId(), "wood_type"));
+    }
+
+    static WoodType woodType(AbstractRegistrar<?> registrar)
+    {
+        return woodType(registrar, UnaryOperator.identity());
+    }
 
     static <R extends AbstractRegistrar<R>, B extends Block> BlockBuilder<R, B, R> wool(R registrar, BlockFactory<B> blockFactory)
     {
@@ -66,12 +79,13 @@ public interface FurnitureSets
         ;
     }
 
-    static <R extends AbstractRegistrar<R>, B extends Block> BlockBuilder<R, B, R> wallLight(R registrar, BlockFactory<B> blockFactory)
+    static <R extends AbstractRegistrar<R>, B extends Block> BlockBuilder<R, B, R> wallLight(R registrar, WoodType woodType, BlockFactory<B> blockFactory)
     {
         return registrar
                 .object("wall_light")
                 .block(blockFactory)
                 .copyInitialPropertiesFrom(() -> Blocks.WALL_TORCH)
+                .sound(woodType.soundType())
                 .lightLevel(14)
                 .blockState((lookup, entry) -> MultiVariantBuilder
                         .builder(
@@ -86,12 +100,13 @@ public interface FurnitureSets
         ;
     }
 
-    static <R extends AbstractRegistrar<R>, B extends Block> BlockBuilder<R, B, R> floorLight(R registrar, BlockFactory<B> blockFactory)
+    static <R extends AbstractRegistrar<R>, B extends Block> BlockBuilder<R, B, R> floorLight(R registrar, WoodType woodType, BlockFactory<B> blockFactory)
     {
         return registrar
                 .object("floor_light")
                 .block(blockFactory)
                 .copyInitialPropertiesFrom(() -> Blocks.TORCH)
+                .sound(woodType.soundType())
                 .lightLevel(14)
                 .blockState((lookup, entry) -> MultiVariantBuilder.builder(entry.value(), Variant.variant().model(ModelLocationUtils.getModelLocation(entry.value()))))
                 .tag(ApexTags.Blocks.PLACEMENT_VISUALIZER)
@@ -99,12 +114,13 @@ public interface FurnitureSets
         ;
     }
 
-    static <R extends AbstractRegistrar<R>, B extends Block> BlockBuilder<R, B, R> stool(R registrar, BlockFactory<B> blockFactory)
+    static <R extends AbstractRegistrar<R>, B extends Block> BlockBuilder<R, B, R> stool(R registrar, WoodType woodType, BlockFactory<B> blockFactory)
     {
         return registrar
                 .object("stool")
                 .block(blockFactory)
                 .copyInitialPropertiesFrom(() -> Blocks.OAK_PLANKS)
+                .sound(woodType.soundType())
                 .blockState((lookup, entry) -> MultiVariantBuilder
                         .builder(
                                 entry.value(),
@@ -118,12 +134,13 @@ public interface FurnitureSets
         ;
     }
 
-    static <R extends AbstractRegistrar<R>, B extends Block> BlockBuilder<R, B, R> cushion(R registrar, BlockFactory<B> blockFactory)
+    static <R extends AbstractRegistrar<R>, B extends Block> BlockBuilder<R, B, R> cushion(R registrar, WoodType woodType, BlockFactory<B> blockFactory)
     {
         return registrar
                 .object("cushion")
                 .block(blockFactory)
                 .copyInitialPropertiesFrom(() -> Blocks.OAK_PLANKS)
+                .sound(woodType.soundType())
                 .blockState((lookup, entry) -> MultiVariantBuilder
                         .builder(
                                 entry.value(),
@@ -137,12 +154,13 @@ public interface FurnitureSets
         ;
     }
 
-    static <R extends AbstractRegistrar<R>, B extends Block> BlockBuilder<R, B, R> paintingSmall(R registrar, BlockFactory<B> blockFactory)
+    static <R extends AbstractRegistrar<R>, B extends Block> BlockBuilder<R, B, R> paintingSmall(R registrar, WoodType woodType, BlockFactory<B> blockFactory)
     {
         return registrar
                 .object("painting_small")
                 .block(blockFactory)
                 .copyInitialPropertiesFrom(() -> Blocks.OAK_PLANKS)
+                .sound(woodType.soundType())
                 .blockState((lookup, entry) -> MultiVariantBuilder
                         .builder(
                                 entry.value(),
@@ -156,12 +174,13 @@ public interface FurnitureSets
         ;
     }
 
-    static <R extends AbstractRegistrar<R>, B extends Block> BlockBuilder<R, B, R> paintingWide(R registrar, BlockFactory<B> blockFactory)
+    static <R extends AbstractRegistrar<R>, B extends Block> BlockBuilder<R, B, R> paintingWide(R registrar, WoodType woodType, BlockFactory<B> blockFactory)
     {
         return registrar
                 .object("painting_wide")
                 .block(blockFactory)
                 .copyInitialPropertiesFrom(() -> Blocks.OAK_PLANKS)
+                .sound(woodType.soundType())
                 .blockState((lookup, entry) -> MultiVariantBuilder
                         .builder(
                                 entry.value(),
@@ -175,12 +194,13 @@ public interface FurnitureSets
         ;
     }
 
-    static <R extends AbstractRegistrar<R>, B extends Block> BlockBuilder<R, B, R> drawer(R registrar, BlockFactory<B> blockFactory)
+    static <R extends AbstractRegistrar<R>, B extends Block> BlockBuilder<R, B, R> drawer(R registrar, WoodType woodType, BlockFactory<B> blockFactory)
     {
         return registrar
                 .object("drawer")
                 .block(blockFactory)
                 .copyInitialPropertiesFrom(() -> Blocks.CHEST)
+                .sound(woodType.soundType())
                 .blockState((lookup, entry) -> MultiVariantBuilder
                         .builder(
                                 entry.value(),
@@ -194,12 +214,13 @@ public interface FurnitureSets
         ;
     }
 
-    static <R extends AbstractRegistrar<R>, B extends BaseBlockComponentHolder> BlockBuilder<R, B, R> shelf(R registrar, BlockFactory<B> blockFactory)
+    static <R extends AbstractRegistrar<R>, B extends BaseBlockComponentHolder> BlockBuilder<R, B, R> shelf(R registrar, WoodType woodType, BlockFactory<B> blockFactory)
     {
         return registrar
                 .object("shelf")
                 .block(blockFactory)
                 .copyInitialPropertiesFrom(() -> Blocks.OAK_PLANKS)
+                .sound(woodType.soundType())
                 .blockState((lookup, entry) -> MultiVariantBuilder.builder(entry.value(), Variant.variant()).with(connectionProperties(entry, ConnectionBlockComponent.SHELF_COMPONENT_TYPE)))
                 .tag(BlockTags.MINEABLE_WITH_AXE)
                 .renderType(() -> RenderType::cutout)
@@ -212,12 +233,13 @@ public interface FurnitureSets
         ;
     }
 
-    static <R extends AbstractRegistrar<R>, B extends BaseBlockComponentHolder> BlockBuilder<R, B, R> sofa(R registrar, BlockFactory<B> blockFactory)
+    static <R extends AbstractRegistrar<R>, B extends BaseBlockComponentHolder> BlockBuilder<R, B, R> sofa(R registrar, WoodType woodType, BlockFactory<B> blockFactory)
     {
         return registrar
                 .object("sofa")
                 .block(blockFactory)
                 .copyInitialPropertiesFrom(() -> Blocks.OAK_PLANKS)
+                .sound(woodType.soundType())
                 .blockState((lookup, entry) -> MultiVariantBuilder.builder(entry.value(), Variant.variant())
                         .with(connectionProperties(entry, ConnectionBlockComponent.SOFA_COMPONENT_TYPE, connectionType -> switch(connectionType) {
                             case INNER_LEFT, INNER_RIGHT -> ModelLocationUtils.getModelLocation(entry.value(), "_corner");
@@ -234,12 +256,13 @@ public interface FurnitureSets
         ;
     }
 
-    static <R extends AbstractRegistrar<R>, B extends Block> BlockBuilder<R, B, R> deskLeft(R registrar, BlockFactory<B> blockFactory)
+    static <R extends AbstractRegistrar<R>, B extends Block> BlockBuilder<R, B, R> deskLeft(R registrar, WoodType woodType, BlockFactory<B> blockFactory)
     {
         return registrar
                 .object("desk_left")
                 .block(blockFactory)
                 .copyInitialPropertiesFrom(() -> Blocks.CHEST)
+                .sound(woodType.soundType())
                 .blockState((lookup, entry) -> MultiVariantBuilder
                         .builder(
                                 entry.value(),
@@ -254,12 +277,13 @@ public interface FurnitureSets
         ;
     }
 
-    static <R extends AbstractRegistrar<R>, B extends Block> BlockBuilder<R, B, R> deskRight(R registrar, BlockFactory<B> blockFactory)
+    static <R extends AbstractRegistrar<R>, B extends Block> BlockBuilder<R, B, R> deskRight(R registrar, WoodType woodType, BlockFactory<B> blockFactory)
     {
         return registrar
                 .object("desk_right")
                 .block(blockFactory)
                 .copyInitialPropertiesFrom(() -> Blocks.CHEST)
+                .sound(woodType.soundType())
                 .blockState((lookup, entry) -> MultiVariantBuilder
                         .builder(
                                 entry.value(),
@@ -274,12 +298,13 @@ public interface FurnitureSets
         ;
     }
 
-    static <R extends AbstractRegistrar<R>, B extends Block> BlockBuilder<R, B, R> chair(R registrar, BlockFactory<B> blockFactory)
+    static <R extends AbstractRegistrar<R>, B extends Block> BlockBuilder<R, B, R> chair(R registrar, WoodType woodType, BlockFactory<B> blockFactory)
     {
         return registrar
                 .object("chair")
                 .block(blockFactory)
                 .copyInitialPropertiesFrom(() -> Blocks.OAK_PLANKS)
+                .sound(woodType.soundType())
                 .blockState((lookup, entry) -> MultiVariantBuilder
                         .builder(
                                 entry.value(),
@@ -294,12 +319,13 @@ public interface FurnitureSets
         ;
     }
 
-    static <R extends AbstractRegistrar<R>, B extends Block> BlockBuilder<R, B, R> bench(R registrar, BlockFactory<B> blockFactory)
+    static <R extends AbstractRegistrar<R>, B extends Block> BlockBuilder<R, B, R> bench(R registrar, WoodType woodType, BlockFactory<B> blockFactory)
     {
         return registrar
                 .object("bench")
                 .block(blockFactory)
                 .copyInitialPropertiesFrom(() -> Blocks.OAK_PLANKS)
+                .sound(woodType.soundType())
                 .blockState((lookup, entry) -> MultiVariantBuilder
                         .builder(
                                 entry.value(),
@@ -313,12 +339,13 @@ public interface FurnitureSets
         ;
     }
 
-    static <R extends AbstractRegistrar<R>, B extends Block> BlockBuilder<R, B, R> bookshelf(R registrar, BlockFactory<B> blockFactory)
+    static <R extends AbstractRegistrar<R>, B extends Block> BlockBuilder<R, B, R> bookshelf(R registrar, WoodType woodType, BlockFactory<B> blockFactory)
     {
         return registrar
                 .object("bookshelf")
                 .block(blockFactory)
                 .copyInitialPropertiesFrom(() -> Blocks.CHEST)
+                .sound(woodType.soundType())
                 .blockState((lookup, entry) -> MultiVariantBuilder
                         .builder(
                                 entry.value(),
@@ -332,12 +359,13 @@ public interface FurnitureSets
         ;
     }
 
-    static <R extends AbstractRegistrar<R>, B extends Block> BlockBuilder<R, B, R> chest(R registrar, BlockFactory<B> blockFactory)
+    static <R extends AbstractRegistrar<R>, B extends Block> BlockBuilder<R, B, R> chest(R registrar, WoodType woodType, BlockFactory<B> blockFactory)
     {
         return registrar
                 .object("chest")
                 .block(blockFactory)
                 .copyInitialPropertiesFrom(() -> Blocks.CHEST)
+                .sound(woodType.soundType())
                 .blockState((lookup, entry) -> MultiVariantBuilder
                         .builder(
                                 entry.value(),
@@ -351,12 +379,13 @@ public interface FurnitureSets
         ;
     }
 
-    static <R extends AbstractRegistrar<R>, B extends Block> BlockBuilder<R, B, R> dresser(R registrar, BlockFactory<B> blockFactory)
+    static <R extends AbstractRegistrar<R>, B extends Block> BlockBuilder<R, B, R> dresser(R registrar, WoodType woodType, BlockFactory<B> blockFactory)
     {
         return registrar
                 .object("dresser")
                 .block(blockFactory)
                 .copyInitialPropertiesFrom(() -> Blocks.CHEST)
+                .sound(woodType.soundType())
                 .blockState((lookup, entry) -> MultiVariantBuilder
                         .builder(
                                 entry.value(),
@@ -370,12 +399,13 @@ public interface FurnitureSets
         ;
     }
 
-    static <R extends AbstractRegistrar<R>, B extends Block> BlockBuilder<R, B, R> wardrobeBottom(R registrar, BlockFactory<B> blockFactory)
+    static <R extends AbstractRegistrar<R>, B extends Block> BlockBuilder<R, B, R> wardrobeBottom(R registrar, WoodType woodType, BlockFactory<B> blockFactory)
     {
         return registrar
                 .object("wardrobe_bottom")
                 .block(blockFactory)
                 .copyInitialPropertiesFrom(() -> Blocks.CHEST)
+                .sound(woodType.soundType())
                 .blockState((lookup, entry) -> MultiVariantBuilder
                         .builder(
                                 entry.value(),
@@ -389,12 +419,13 @@ public interface FurnitureSets
         ;
     }
 
-    static <R extends AbstractRegistrar<R>, B extends Block> BlockBuilder<R, B, R> wardrobeTop(R registrar, BlockFactory<B> blockFactory)
+    static <R extends AbstractRegistrar<R>, B extends Block> BlockBuilder<R, B, R> wardrobeTop(R registrar, WoodType woodType, BlockFactory<B> blockFactory)
     {
         return registrar
                 .object("wardrobe_top")
                 .block(blockFactory)
                 .copyInitialPropertiesFrom(() -> Blocks.CHEST)
+                .sound(woodType.soundType())
                 .blockState((lookup, entry) -> MultiVariantBuilder
                         .builder(
                                 entry.value(),
@@ -410,24 +441,26 @@ public interface FurnitureSets
         ;
     }
 
-    static <R extends AbstractRegistrar<R>, B extends Block> BlockBuilder<R, B, R> chandelier(R registrar, BlockFactory<B> blockFactory)
+    static <R extends AbstractRegistrar<R>, B extends Block> BlockBuilder<R, B, R> chandelier(R registrar, WoodType woodType, BlockFactory<B> blockFactory)
     {
         return registrar
                 .object("chandelier")
                 .block(blockFactory)
                 .copyInitialPropertiesFrom(() -> Blocks.OAK_PLANKS)
+                .sound(woodType.soundType())
                 .lightLevel(14)
                 .blockState((lookup, entry) -> MultiVariantBuilder.builder(entry.value(), Variant.variant().model(ModelLocationUtils.getModelLocation(entry.value()))))
                 .defaultItem()
         ;
     }
 
-    static <R extends AbstractRegistrar<R>, B extends Block> BlockBuilder<R, B, R> lockbox(R registrar, BlockFactory<B> blockFactory)
+    static <R extends AbstractRegistrar<R>, B extends Block> BlockBuilder<R, B, R> lockbox(R registrar, WoodType woodType, BlockFactory<B> blockFactory)
     {
         return registrar
                 .object("lockbox")
                 .block(blockFactory)
                 .copyInitialPropertiesFrom(() -> Blocks.CHEST)
+                .sound(woodType.soundType())
                 .blockState((lookup, entry) -> MultiVariantBuilder
                         .builder(
                                 entry.value(),
@@ -441,12 +474,13 @@ public interface FurnitureSets
         ;
     }
 
-    static <R extends AbstractRegistrar<R>, B extends BaseBlockComponentHolder> BlockBuilder<R, B, R> counter(R registrar, BlockFactory<B> blockFactory)
+    static <R extends AbstractRegistrar<R>, B extends BaseBlockComponentHolder> BlockBuilder<R, B, R> counter(R registrar, WoodType woodType, BlockFactory<B> blockFactory)
     {
         return registrar
                 .object("counter")
                 .block(blockFactory)
                 .copyInitialPropertiesFrom(() -> Blocks.OAK_PLANKS)
+                .sound(woodType.soundType())
                 .blockState((lookup, entry) -> MultiVariantBuilder.builder(entry.value(), Variant.variant())
                         .with(connectionProperties(entry, ConnectionBlockComponent.COUNTER_COMPONENT_TYPE, connectionType -> switch(connectionType) {
                             case INNER_LEFT, INNER_RIGHT, OUTER_LEFT, OUTER_RIGHT -> ModelLocationUtils.getModelLocation(entry.value(), "_corner");
